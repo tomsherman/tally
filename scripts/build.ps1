eba1b88b05309d98abb38c15345291fcfc468e23
Jack Guo <tom.sherman@replicant.ai>
2025-07-14 09:23:56 -0400

    create build scripts

---
+++ scripts/build.ps1
@@ -0,0 +1,13 @@
+# Create virtual environment if it doesn't exist
+if (!(Test-Path "../venv")) {
+    python -m venv ../venv
+}
+
+# Use virtual environment
+../venv/Scripts/Activate.ps1
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Create an executable
+pyinstaller --name tally src/tally/cli.py
---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "../venv" ]; then
+    python3 -m venv ../venv
+fi
+
+# Use virtual environment
+source ../venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Create an executable
+pyinstaller --name tally src/tally/cli.py

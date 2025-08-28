12b4eabed34285d74ffe382c3e9dd6613a9d05d9
Jack Guo <tom.sherman@replicant.ai>
2025-08-27 21:14:25 -0400

    fix build script venv path

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,10 +1,12 @@
+# Call from the parent directory with `.\scripts\build.ps1`
+
 # Create virtual environment if it doesn't exist
-if (!(Test-Path "../venv")) {
-    python -m venv ../venv
+if (!(Test-Path ".\venv")) {
+    python -m venv .\venv
 }
 
 # Use virtual environment
-../venv/Scripts/Activate.ps1
+.\venv\Scripts\Activate.ps1
 
 # Install dependencies
 pip install -r requirements.txt
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Call from the parent directory with `./scripts/build.sh`
+
 # Create virtual environment if it doesn't exist
-if [ ! -d "../venv" ]; then
-    python3 -m venv ../venv
+if [ ! -d "./venv" ]; then
+    python3 -m venv ./venv
 fi
 
 # Use virtual environment
-source ../venv/bin/activate
+source ./venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
